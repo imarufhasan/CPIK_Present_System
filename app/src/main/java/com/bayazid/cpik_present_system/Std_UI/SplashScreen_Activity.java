5c3a2d85134bc16f9997367fe2617482bfafb889
MBayazid <musanna324@gmail.com>
2019-06-24 16:41:31 +0100

    Basic Structure

---
+++ app/src/main/java/com/bayazid/cpik_present_system/Std_UI/SplashScreen_Activity.java
@@ -0,0 +1,15 @@
+package com.bayazid.cpik_present_system.Std_UI;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bayazid.cpik_present_system.R;
+
+public class SplashScreen_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen_);
+    }
+}
